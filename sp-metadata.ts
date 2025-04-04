e669f24d8218fd4f3fd93138040965cbe21aed6c
adamreaksmey <adaminiature@gmail.com>
2025-04-04 08:41:05 +0700

    fixed meta data

--- sp-metadata.ts
+++ sp-metadata.ts
@@ -1,15 +1,13 @@
-import { ServiceProviderMetadata } from 'passport-saml';
 import type { SamlConfig } from 'passport-saml';
 import * as fs from 'fs';
 
-const samlConfig: SamlOptions = {
+const samlConfig: SamlConfig = {
   callbackUrl: 'http://localhost:3000/auth/saml/callback',
   entryPoint: 'https://idp.ssocircle.com/sso/SSOPOST/metaAlias/publicidp',
   issuer: 'nestjs-saml-demo',
   cert: fs.readFileSync('./cert.pem', 'utf8'),
   privateCert: fs.readFileSync('./key.pem', 'utf8'),
-  
-};
+} as any;
 
 const { getSamlMetadata } = require('passport-saml-metadata');
 
